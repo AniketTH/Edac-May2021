36a00aea331d3ef27c2e73014a5a55233f07ec29
AniketTH <83489172+AniketTH@users.noreply.github.com>
2021-05-07 22:52:01 +0530

    Add files via upload

---
+++ Assignments/question2.java
@@ -0,0 +1,11 @@
+class question2
+{
+public static void main(String args[])
+{
+int a=74;
+int b=36;
+int c;
+c=a+b;
+System.out.println("Addition is = "+c);
+}
+}
---
+++ Assignments/question9.java
@@ -0,0 +1,17 @@
+class question9
+{
+public static void main(String args[])
+{
+double a=25.5d;
+double b=3.5d;
+double c=40.5d;
+double d=4.5d;
+double e;
+
+e= ((a*b-b*b)/(c-d));
+System.out.println("The result is = "+e);
+
+
+
+}
+}
